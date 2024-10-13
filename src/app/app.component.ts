bbaf11b98c940423df6753b0a0e4efe6b8663c45
Juan Camilo Cuenca Sepulveda <camilocuenca1810@gmail.com>
2024-10-12 21:02:50 -0500

    feat(component): create the first version of the Navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {RouterModule} from '@angular/router'
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component'; // Importa tu NavbarComponent
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,RouterModule],
+  standalone: true,  // Habilita el componente standalone
+  imports: [RouterOutlet, RouterModule, NavbarComponent],  // Incluye los imports necesarios
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // Corrección: styleUrls (en plural)
 })
 export class AppComponent {
   title = 'my-app';
-  footer = 'Universidad del Quindío - 2024-2'
+  footer = 'Universidad del Quindío - 2024-2';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 export const routes: Routes = [
    { path: '', component: InicioComponent },
    { path: 'login', component: LoginComponent },
    { path: 'registro', component: RegistroComponent },
+   { path: 'navbar', component: NavbarComponent },
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
