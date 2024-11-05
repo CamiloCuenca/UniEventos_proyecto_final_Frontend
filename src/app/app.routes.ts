c0220ad0dd95bb8ec1f186c0eb91ea255048824a
Brandon Montealegre <sneid123@hotmail.com>
2024-11-05 11:41:51 -0500

    Se implemento las validaciones del registro, tambien una parte para la recuperacion de la clave

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,12 @@ import {HomeAdminComponent} from './pages/home-admin/home-admin.component'
 import { RolesGuard } from './services/roles.service';
 import { GestionarEventosComponent } from './pages/gestionar-eventos/gestionar-eventos.component';
 import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component';
+import { VerifyCodeComponent } from './pages/verify-code/verify-code.component';
 
 export const routes: Routes = [
    { path: '', component: InicioComponent },
    { path: 'recover-password', component: RecoverPasswordComponent},
+   { path: 'verify-code', component: VerifyCodeComponent},
    { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
    { path: 'registro', component: RegistroComponent, canActivate: [LoginGuard] },
    {path:'perfil',component:ProfileComponent},
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -49,7 +49,42 @@ export class AuthService {
 
 
   crearCuenta(datos: any): Observable<any> {
-    return this.http.post(this.apiUrl, datos);
+    return this.http.post('http://localhost:8080/api/auth/cuenta/crear-cuenta', datos).pipe(
+      catchError(this.handleError) // Captura y maneja errores
+    );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Ocurrió un error inesperado.';
+  
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Error del lado del servidor
+      if (error.error && error.error.message) {
+        // Asegúrate de que el backend devuelva un mensaje en error.error.message
+        errorMessage = error.error.message; // Extraer el mensaje específico del cuerpo de la respuesta
+      } else {
+        switch (error.status) {
+          case 409: // Conflicto: Email ya existe
+            errorMessage = 'El correo electrónico ya está en uso.';
+            break;
+          case 400: // Solicitud incorrecta: Cédula ya existe
+            errorMessage = 'El número de identificación ya está en uso.';
+            break;
+          default:
+            errorMessage = 'Error en la solicitud.';
+            break;
+        }
+      }
+    }
+  
+    return throwError(() => new Error(errorMessage));
+  }
+
+
+
+
 }
 
