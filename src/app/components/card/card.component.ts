88c2b450d7f8fe884cef93c3d90aaa3cacd0ab5d
BrandonMontealegre <sneid123@hotmail.com>
2024-11-15 02:17:00 -0500

    se acomodo mejor la pagina

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,7 +9,8 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
   @Input() eventImage: string = '';   // URL de la imagen del evento
-  @Input() eventTitle: string = '';    // Título del evento
+  @Input() eventTitle: string = '';   // Título del evento
+  @Input() eventCity: string = '';    
   @Input() eventDate: string | null = null;  // Fecha del evento, puede ser null
   @Input() eventLocation: string | null = null; // Permitir null como un valor válido
 }
