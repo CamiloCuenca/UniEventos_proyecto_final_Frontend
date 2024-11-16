528cbe6b32bdb9713c229b688a4ed49dd9d7c49e
CamiloCuenca <camilocuenca1810@gmail.com>
2024-11-15 22:47:30 -0500

    Se aplicaron algunos estilos

--- src/app/components/carrucel/carrucel.component.ts
+++ src/app/components/carrucel/carrucel.component.ts
@@ -11,8 +11,9 @@ import { Component } from '@angular/core';
 })
 export class CarrucelComponent {
   images: string[] = [
-    'https://firebasestorage.googleapis.com/v0/b/unieventos-1c779.appspot.com/o/Imagenes%20carrucel%2F240808100445628_estelar_estelar.jpg?alt=media&token=8a426190-8834-41f2-9e91-ba4cf83810ea',
-    "https://firebasestorage.googleapis.com/v0/b/unieventos-1c779.appspot.com/o/Imagenes%20carrucel%2F240724122710489_performer_img_CAT1.jpg?alt=media&token=ca4037fb-06ef-4d2a-8d4d-55125bb52bf8"
+    'https://firebasestorage.googleapis.com/v0/b/unieventos-1c779.appspot.com/o/Imagenes%20carrucel%2F4ce24f2700795f049082f8a31f8ec6c9.gif?alt=media&token=db9c46f6-2660-4e73-812a-036533ad501f',
+    'https://firebasestorage.googleapis.com/v0/b/unieventos-1c779.appspot.com/o/Imagenes%20carrucel%2Fcdd3dfca2535b350e6060473143245ee.gif?alt=media&token=a4ad3df8-ae77-48a0-b17e-f04d3d7a9389',
+    'https://firebasestorage.googleapis.com/v0/b/unieventos-1c779.appspot.com/o/Imagenes%20carrucel%2F103769bee25bd3a4d18c572c2834e0a4.gif?alt=media&token=ab7f62d6-0c2e-4b0e-bf78-5838a9620ad5'
   ];
 
   currentImageIndex = 0;
